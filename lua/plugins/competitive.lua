925d53998a626fecfb33bd2c27d222b0482290ae
SamiulBasirFahim <samiulbasirfahim.rxen@gmail.com>
2024-10-24 07:28:16 +0600

    changed a bit

--- lua/plugins/competitive.lua
+++ lua/plugins/competitive.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"xeluxee/competitest.nvim",
-		dependencies = "MunifTanjim/nui.nvim",
-		init = function()
-			require("competitest").setup({
-				testcases_directory = ".testcases",
-			})
-		end,
-		ft = "cpp",
-	},
-	{
-		"kawre/leetcode.nvim",
-		build = ":TSUpdate html",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim", -- required by telescope
-			"MunifTanjim/nui.nvim",
+    {
+        "xeluxee/competitest.nvim",
+        dependencies = "MunifTanjim/nui.nvim",
+        init = function()
+            require("competitest").setup({
+                testcases_directory = ".testcases",
+            })
+        end,
+        ft = "cpp",
+    },
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
 
-			-- optional
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			lang = "cpp",
-			storage = {
-				home = "~/dev/problemSolving/leetcode",
-				cache = vim.fn.stdpath("cache") .. "/leetcode",
-			},
-		},
-	},
+            -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            lang = "cpp",
+            storage = {
+                home = "~/dev/leetcode",
+                cache = vim.fn.stdpath("cache") .. "/leetcode",
+            },
+        },
+    },
 }
